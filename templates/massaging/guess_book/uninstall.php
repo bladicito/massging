7a7f2dd6d5ab4050b20867ade4dcd796e7849621
Bladimir Ardiles <bladi_sys@hotmail.com>
2015-08-06 22:43:34 +0200

    remove test files

--- templates/massaging/guess_book/uninstall.php
+++
@@ -1,8 +0,0 @@
-<?php
-if (! defined('WB_PATH')) { die('Cannot access this file directly'); }
-
-$oReg = WbAdaptor::getInstance();
-
-
-$oSqlStruct = new SqlImport($oReg->Db, __DIR__.'/install-struct.sql');
-$oSqlStruct->doImport(__FILE__);
