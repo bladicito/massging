bd18e132782ba383120ad975ff8d83201165a911
Bladimir Ardiles <bladi_sys@hotmail.com>
2015-08-09 12:36:51 +0200

    include module form to the repository as changes need to be done.

--- templates/massaging/index.php
+++ templates/massaging/index.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <link rel="icon" href="<?php echo TEMPLATE_DIR;?>/favicon.png">
         <meta name="description" content="<?php page_description(); ?>" />
         <title><?php page_title(); ?></title>
         <meta name="keywords" content="<?php page_keywords(); ?>" />
