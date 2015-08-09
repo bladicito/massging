b5fb2152b5bac9114ed13bf6c682fe45edab0dc8
Bladimir Ardiles <bladi_sys@hotmail.com>
2015-08-09 19:39:31 +0200

    form module was updated.

--- templates/massaging/js/scripts.js
+++ templates/massaging/js/scripts.js
@@ -3822,6 +3822,51 @@
 }));
 (function($) {
 
+	Tc.Module.Logo = Tc.Module.extend({
+
+		/**
+		 * Initializes the Default module.
+		 *
+		 * @method init
+		 * @constructor
+		 * @param {jQuery|Zepto} $ctx the jquery context
+		 * @param {Sandbox} sandbox the sandbox to get the resources from
+		 * @param {String} modId the unique module id
+		 */
+		init: function($ctx, sandbox, modId) {
+			// call base constructor
+			this._super($ctx, sandbox, modId);
+		},
+
+		/**
+		 * Hook function to do all of your module stuff.
+		 *
+		 * @method on
+		 * @param {Function} callback function
+		 * @return void
+		 */
+		on: function(callback) {
+			callback();
+
+			var mod = this,
+				$ctx = mod.ctx
+			;
+
+
+		},
+
+		/**
+		 * Hook function to trigger your events.
+		 *
+		 * @method after
+		 * @return void
+		 */
+		after: function() {
+		}
+	});
+})(Tc.$);
+(function($) {
+
     Tc.Module.Form = Tc.Module.extend({
 
         /**
@@ -3919,51 +3964,6 @@
         after: function() {
         }
     });
-})(Tc.$);
-(function($) {
-
-	Tc.Module.Logo = Tc.Module.extend({
-
-		/**
-		 * Initializes the Default module.
-		 *
-		 * @method init
-		 * @constructor
-		 * @param {jQuery|Zepto} $ctx the jquery context
-		 * @param {Sandbox} sandbox the sandbox to get the resources from
-		 * @param {String} modId the unique module id
-		 */
-		init: function($ctx, sandbox, modId) {
-			// call base constructor
-			this._super($ctx, sandbox, modId);
-		},
-
-		/**
-		 * Hook function to do all of your module stuff.
-		 *
-		 * @method on
-		 * @param {Function} callback function
-		 * @return void
-		 */
-		on: function(callback) {
-			callback();
-
-			var mod = this,
-				$ctx = mod.ctx
-			;
-
-
-		},
-
-		/**
-		 * Hook function to trigger your events.
-		 *
-		 * @method after
-		 * @return void
-		 */
-		after: function() {
-		}
-	});
 })(Tc.$);
 (function($) {
     'use strict';
---
+++ templates/massaging/modules-terrific/form/js/form.js
@@ -0,0 +1,100 @@
+(function($) {
+
+    Tc.Module.Form = Tc.Module.extend({
+
+        /**
+         * Initializes the Default module.
+         *
+         * @method init
+         * @constructor
+         * @param {jQuery|Zepto} $ctx the jquery context
+         * @param {Sandbox} sandbox the sandbox to get the resources from
+         * @param {String} modId the unique module id
+         */
+        init: function($ctx, sandbox, modId) {
+            // call base constructor
+            this._super($ctx, sandbox, modId);
+        },
+
+        /**
+         * Hook function to do all of your module stuff.
+         *
+         * @method on
+         * @param {Function} callback function
+         * @return void
+         */
+        on: function(callback) {
+            callback();
+
+            var mod = this,
+                $ctx = mod.ctx
+                ;
+
+            mod.$submitForm = $('.js-submit-form', $ctx);
+            mod.$inputs     = $('.form-control', $ctx);
+            mod.$form       = $('form', $ctx);
+            mod.$mandatoryFields = (function() {
+                var $fields = $('.frm-required', $ctx).siblings('input'),
+                    arrayWithItems =  []
+                ;
+
+                $fields.each(function(index, item) {
+                    arrayWithItems.push({
+                        input   : $(item),
+                        wrapper : $(item).closest('.form-group')
+                    });
+
+                });
+
+                return $(arrayWithItems);
+
+            })();
+
+            mod.$submitForm.on('click', function(evt) {
+                evt.preventDefault();
+                mod.checkForm();
+            });
+
+            mod.$inputs.on('focus', function() {
+                $(this).closest('.form-group').removeClass('has-error');
+            });
+
+
+
+
+        },
+
+        checkForm: function() {
+           var mod = this,
+               errors = []
+           ;
+
+            mod.$mandatoryFields.each(function(index, item) {
+                if (item.input.val() === '') {
+                    errors.push(item);
+                }
+            });
+
+
+            if (errors.length > 1) {
+                $(errors).each(function(index, item) {
+                    item.wrapper.addClass('has-error');
+                });
+
+                return this;
+            }
+
+           mod.$form.submit();
+
+        },
+
+        /**
+         * Hook function to trigger your events.
+         *
+         * @method after
+         * @return void
+         */
+        after: function() {
+        }
+    });
+})(Tc.$);
